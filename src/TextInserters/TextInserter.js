364b3672eebddca8a2cea79b7c37ebf87a40caa5
tamirshina <tamirshina@gmail.com>
2020-04-05 20:43:11 +0300

    css

--- src/TextInserters/TextInserter.js
+++ src/TextInserters/TextInserter.js
@@ -11,64 +11,66 @@ import upperTextArrow from '../assets/11_textArrowUP.png';
 import hebrewHeader from '../assets/23_title_HEB.png';
 import '../css/Styles.css';
 
-function TextInserter (){
+function TextInserter() {
 
     const lang = useContext(LangContext).lang;
     const textParaEl = useRef(null);
     const upperScrollEl = useRef(null);
     const bottomScrollEl = useRef(null);
 
-    function whichFileToUse (){
-        if(lang==="hebrew"){
+    function createMarkup(str) { return { __html: str } };
+
+    function whichFileToUse() {
+        if (lang === "hebrew") {
             return JSON.parse(JSON.stringify(hebrewText.frontPage));
         }
-        if(lang==="english"){
+        if (lang === "english") {
             return JSON.parse(JSON.stringify(englishText.frontPage));
         }
-        else{
+        else {
             return JSON.parse(JSON.stringify(russianText.frontPage));
         }
     }
-    const  scrollAndUpdateDown=()=> {
+    const scrollAndUpdateDown = () => {
 
-        let maxTextLength =textParaEl.current.scrollHeight-textParaEl.current.clientHeight;
-        textParaEl.current.scrollTop+=10;
+        let maxTextLength = textParaEl.current.scrollHeight - textParaEl.current.clientHeight;
+        textParaEl.current.scrollTop += 10;
 
-        if(textParaEl.current.scrollTop!== 0){
+        if (textParaEl.current.scrollTop !== 0) {
             upperScrollEl.current.style.visibility = 'visible';
         }
-        if(textParaEl.current.scrollTop===maxTextLength){
+        if (textParaEl.current.scrollTop === maxTextLength) {
             bottomScrollEl.current.style.visibility = 'hidden';
         }
     }
 
-    const  scrollAndUpdateUp=()=> {
+    const scrollAndUpdateUp = () => {
 
-        
-        textParaEl.current.scrollTop-=10;
 
-        if(textParaEl.current.scrollTop=== 0){
+        textParaEl.current.scrollTop -= 10;
+
+        if (textParaEl.current.scrollTop === 0) {
             upperScrollEl.current.style.visibility = 'hidden';
-        }if(bottomScrollEl.current.style.visibility==='hidden'){
+        } if (bottomScrollEl.current.style.visibility === 'hidden') {
             bottomScrollEl.current.style.visibility = 'visible';
         }
     }
- 
-  return (
-    <>
-    <HandArrowFront />
-    <div className={isLeftToRight()?'leftToRightTextBox':'textBoxCssFront'}>
-    {isLeftToRight()?
-    <FrontTitleft />:
-          <img alt='hebHeader' src={hebrewHeader} className='frontPageHeTitle'/>}
-          <img  ref={upperScrollEl} onClick={scrollAndUpdateUp} id="scrollBtnPng" src={upperTextArrow} alt="scrollBtn" className={isLeftToRight()?'topScrollOneEN':'topScrollOneHE'}/>
-            <p ref={textParaEl} className={isLeftToRight()?'lefToRightTexstCss':'textHeFront'} id="openingTextBox"> 
-                {whichFileToUse()}
-            </p>
-            <img ref={bottomScrollEl} onClick={scrollAndUpdateDown} id="scrollBtnPng" src={scrollBtn} alt="scrollBtn" className={isLeftToRight()?'buttomScrollOneEN':'buttomScrollOneHE'}/>
-        </div>
-    </>
-        );
+
+    return (
+        <>
+            <HandArrowFront />
+            <div className={isLeftToRight() ? 'leftToRightTextBox' : 'textBoxCssFront'}>
+                {isLeftToRight() ?
+                    <FrontTitleft /> :
+                    <img alt='hebHeader' src={hebrewHeader} className='frontPageHeTitle' />}
+                <img ref={upperScrollEl} onClick={scrollAndUpdateUp} id="scrollBtnPng" src={upperTextArrow} alt="scrollBtn" className={isLeftToRight() ? 'topScrollOneEN' : 'topScrollOneHE'} />
+                <p ref={textParaEl} className={isLeftToRight() ? 'lefToRightTexstCss' : 'textHeFront'} id="openingTextBox" dangerouslySetInnerHTML={createMarkup(whichFileToUse())}>
+
+                </p>
+                <img ref={bottomScrollEl} onClick={scrollAndUpdateDown} id="scrollBtnPng" src={scrollBtn} alt="scrollBtn" className={isLeftToRight() ? 'buttomScrollOneEN' : 'buttomScrollOneHE'} />
+            </div>
+        </>
+    );
 }
 
 export default TextInserter;
